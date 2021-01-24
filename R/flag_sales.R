c264f90a55809413d44fcef3c5836d71370c6ab1
Dan Snow <snow.dfs@gmail.com>
2021-01-24 13:23:57 -0600

    Update sales flagging script to work with package

--- R/flag_sales.R
+++ R/flag_sales.R
@@ -7,6 +7,7 @@ library(arrow)
 library(ccao)
 library(dplyr)
 library(ggplot2)
+library(glue)
 library(here)
 library(purrr)
 library(recipes)
@@ -15,8 +16,20 @@ library(stringr)
 library(tidymodels)
 library(treesnip)
 
-# Load helper functions from file
-source("R/model_funs.R")
+## Flagging variables
+
+# Factor by which ratios must deviate from 1 in order to be flagged. For
+# example, setting this to 4 will flag ratios higher than 4 and less than 0.25
+flag_ratio_factor <- 4
+
+# Factor by which predicted value deviates from the neighborhood median. For
+# example, if a neighborhood's median sale price is $100K and the model predicts
+# a value of $400K, that observation's nbhd factor will be 4
+flag_nbhd_factor <- 4
+
+# Factor by which sales for the same PIN deviate year-over-year. The higher the
+# factor the larger the deviation
+flag_yoy_pct_change_factor <- 1.6
 
 
 
@@ -26,18 +39,16 @@ source("R/model_funs.R")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Load the final lightgbm model object and recipe from file
-lgbm_final_full_fit <- model_load(here("output", "models", "lgbm_model.zip"))
-lgbm_final_full_recipe <- readRDS(here("output", "models", "lgbm_recipe.rds"))
+lgbm_final_full_fit <- ccao::model_lgbm_load(
+  here("output", "models", "lgbm_model.zip")
+)
+lgbm_final_full_recipe <- readRDS(
+  here("output", "models", "lgbm_recipe.rds")
+)
 
 # Get all arms-length sales and clean up some string columns
 sales_data <- read_parquet(here("input", "modeldata.parquet")) %>%
-  filter(ind_arms_length) %>%
-  mutate(
-    meta_pin = str_pad(meta_pin, 14, "left", "0"),
-    meta_class = str_pad(meta_class, 3, "left", "0"),
-    meta_multi_code = as.character(meta_multi_code),
-    meta_document_num = str_pad(meta_document_num, 11, "left", "0")
-  )
+  filter(ind_arms_length)
 
 # Generate predicted values for all sales using the saved model and recipe
 sales_data <- sales_data %>%
@@ -48,6 +59,21 @@ sales_data <- sales_data %>%
       data = .
     )
   )
+
+# Select relevant output columns and calculate sales ratio
+sales_filtered <- sales_data %>%
+  select(
+    meta_pin, meta_document_num, meta_class, meta_year, meta_town_code, meta_nbhd,
+    geo_property_address, geo_property_apt_no, geo_property_city, geo_property_zip,
+    geo_longitude, geo_latitude, model_value = lgbm, meta_sale_price
+  ) %>%
+  mutate(sales_ratio = model_value / meta_sale_price) %>%
+  group_by(meta_nbhd) %>%
+  mutate(
+    sales_ratio_from_nbhd_med =
+      model_value / median(meta_sale_price, na.rm = TRUE)
+  ) %>% 
+  ungroup()
 
 
 
@@ -61,43 +87,47 @@ sales_data <- sales_data %>%
 # hand-reviewed by the CCAO and excluded from the model if they are atypical or
 # not arms-length. Not all sales flagged here will be excluded
 
-# Select relevant output columns and calculate sales ratio
-sales_filtered <- sales_data %>%
-  select(
-    meta_pin, meta_document_num, meta_class, meta_year, meta_town_code, meta_nbhd,
-    geo_property_address, geo_property_apt_no, geo_property_city, geo_property_zip,
-    geo_longitude, geo_latitude, model_value = lgbm, meta_sale_price
-  ) %>%
-  mutate(sales_ratio = model_value / meta_sale_price) %>%
-  group_by(meta_nbhd) %>%
-  mutate(nbhd_avg_pred_ratio = model_value / mean(meta_sale_price)) %>% 
-  ungroup()
-
 # Flag very high and very low ratios (anything that deviates from its prediction
-# be a factor of 3 or more)
+# be a factor of flag_ratio_factor or more)
 ratio_factor <- sales_filtered %>%
-  filter(sales_ratio < 0.25 | sales_ratio > 4) %>%
-
+  filter(
+    sales_ratio < (1 / flag_ratio_factor) |
+    sales_ratio > flag_ratio_factor
+  ) %>%
   # Invert ratios in order to rank by magnitude of error
   mutate(
-    adj_sales_ratio = ifelse(sales_ratio < 1, 1 / sales_ratio, sales_ratio),
-    reason = "Estimate differs from sale price by factor of 3 or greater"
+    adj_sales_ratio = ifelse(
+      sales_ratio < 1,
+      1 / sales_ratio,
+      sales_ratio
+    ),
+    reason = glue(
+      "Estimate differs from sale price",
+      "by factor of {flag_ratio_factor} or greater"
+    )
   ) %>%
   arrange(desc(adj_sales_ratio)) %>%
   mutate(rank = row_number()) %>%
   select(-adj_sales_ratio)
 
-# Flag large deviations from the neighborhood average sale price
-nbhd_avg_factor <- sales_filtered %>%
+# Flag large deviations from the neighborhood median sale price
+nbhd_factor <- sales_filtered %>%
   filter(
-    (nbhd_avg_pred_ratio < 0.25 | nbhd_avg_pred_ratio > 4),
+    (sales_ratio_from_nbhd_med < (1 / flag_nbhd_factor) |
+    sales_ratio_from_nbhd_med > flag_nbhd_factor),
     !meta_pin %in% ratio_factor
   ) %>%
-
   # Invert ratios in order to rank by magnitude of error
   mutate(
-    avg_nbhd_ratio = ifelse(nbhd_avg_pred_ratio < 1, 1 / nbhd_avg_pred_ratio, nbhd_avg_pred_ratio),
-    reason = "Estimate differs from neighborhood average sale price by factor of 3 or greater"
+    avg_nbhd_ratio = ifelse(
+      sales_ratio_from_nbhd_med < 1,
+      1 / sales_ratio_from_nbhd_med,
+      sales_ratio_from_nbhd_med
+    ),
+    reason = glue(
+      "Estimate differs from neighborhood average",
+      "sale price by factor of {flag_nbhd_factor} or greater"
+    )
   ) %>%
   arrange(desc(avg_nbhd_ratio)) %>%
   mutate(rank = row_number()) %>%
@@ -106,39 +136,41 @@ nbhd_avg_factor <- sales_filtered %>%
 # Flag properties with a high year-over-year change in sale price
 yoy_change_factor <- sales_filtered %>%
   group_by(meta_pin) %>%
-
   # Calculate ratio between range of sale prices and their mean for each PIN
   mutate(
-    r = max(meta_sale_price) - min(meta_sale_price),
-    m = mean(meta_sale_price),
-    dev = r / m
+    dev = (max(meta_sale_price) - min(meta_sale_price)) / mean(meta_sale_price)
   ) %>%
-  select(-r, -m) %>%
   ungroup() %>%
   mutate(reason = "High year-over-year change in sale price within 5 years") %>%
-
   # Filter for large YoY changes / large deviations from the mean sale price
   filter(
-    dev > 1.6,
+    dev > flag_yoy_pct_change_factor,
     !meta_pin %in% ratio_factor$meta_pin,
-    !meta_pin %in% nbhd_avg_factor$meta_pin
+    !meta_pin %in% nbhd_factor$meta_pin
   ) %>%
   arrange(desc(dev)) %>%
   mutate(rank = row_number()) %>%
   select(-dev)
 
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##### Cleanup and Save ####
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 # Combine flagged values into a single data frame and clean up the output
 # for export to CSV/Excel
 flagged_sales <- bind_rows(
   ratio_factor,
-  nbhd_avg_factor,
+  nbhd_factor,
   yoy_change_factor
 ) %>%
   mutate(
     meta_pin = pin_format_pretty(meta_pin),
     `Township Name` = town_convert(meta_town_code)
   ) %>%
-  select(-nbhd_avg_pred_ratio) %>%
+  select(-sales_ratio_from_nbhd_med) %>%
   relocate(`Township Name`, .after = "meta_year") %>%
   vars_recode(type = "long") %>%
   vars_rename(names_from = "standard", names_to = "pretty") %>%
@@ -163,7 +195,7 @@ write.csv(
 flagged_sales %>%
   filter(!is.na(Latitude) & `Severity Rank` <= 1000) %>%
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
-  ggplot() +
+ggplot() +
   geom_sf(data = town_shp) +
   geom_sf(aes(geometry = geometry), color = "blue", alpha = 0.2) +
   theme_void() +
