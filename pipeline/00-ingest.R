8cf0cfaa3f3c73363b85946803ea4cacc4580a3b
Dan Snow <daniel.snow@cookcountyil.gov>
2022-02-16 20:58:32 -0600

    DVC-ify ingest stage

--- pipeline/00-ingest.R
+++ pipeline/00-ingest.R
@@ -17,10 +17,14 @@ library(lubridate)
 library(RJDBC)
 library(tictoc)
 library(tidyr)
+library(yaml)
 source(here("R", "helpers.R"))
 
-# Initialize a dictionary of file paths and URIs. See R/file_dict.csv
+# Initialize a dictionary of file paths. See R/file_dict.csv for details
 paths <- model_file_dict()
+
+# Load the parameters file containing the run settings
+params <- read_yaml("params.yaml")
 
 # Setup the Athena JDBC driver
 aws_athena_jdbc_driver <- RJDBC::JDBC(
@@ -35,40 +39,6 @@ AWS_ATHENA_CONN_JDBC <- dbConnect(
   url = Sys.getenv("AWS_ATHENA_JDBC_URL"),
   aws_credentials_provider_class = Sys.getenv("AWS_CREDENTIALS_PROVIDER_CLASS"),
   Schema = "Default"
-)
-
-# Set the assessment year of the data (data year to base assessment on)
-model_assessment_data_year <- Sys.getenv(
-  "MODEL_ASSESSMENT_DATA_YEAR",
-  unset = lubridate::year(Sys.Date()) - 1
-)
-
-# Get the year of assessment
-model_assessment_year <- Sys.getenv(
-  "MODEL_ASSESSMENT_YEAR",
-  unset = lubridate::year(Sys.Date())
-)
-
-# Set the assessment date, usually Jan 1st
-model_assessment_date <- Sys.getenv(
-  "MODEL_ASSESSMENT_DATE",
-  unset = lubridate::make_date(lubridate::year(Sys.Date()))
-)
-
-# Get the minimum and maximum years to use for the training data (sales) sample
-model_min_sale_year <- Sys.getenv(
-  "MODEL_MIN_SALE_YEAR",
-  unset = as.numeric(model_assessment_data_year) - 7
-)
-model_max_sale_year <- Sys.getenv(
-  "MODEL_MAX_SALE_YEAR",
-  unset = as.numeric(model_assessment_data_year)
-)
-
-# Whether or not to generate a new set of townhome complex identifiers
-model_generate_complex_id <- Sys.getenv(
-  "MODEL_GENERATE_COMPLEX_ID",
-  unset = FALSE
 )
 
 
@@ -93,8 +63,8 @@ training_data <- dbGetQuery(
       ON sale.pin = res.meta_pin
       AND sale.year = res.meta_year
   WHERE (res.meta_year 
-      BETWEEN '{model_min_sale_year}' 
-      AND '{model_max_sale_year}')
+      BETWEEN '{params$input$min_sale_year}' 
+      AND '{params$input$max_sale_year}')
   AND ((sale.sale_price_log10
       BETWEEN sale.sale_filter_lower_limit
       AND sale.sale_filter_upper_limit)
@@ -111,7 +81,7 @@ assessment_data <- dbGetQuery(
   conn = AWS_ATHENA_CONN_JDBC, glue("
   SELECT *
   FROM model.vw_card_res_input
-  WHERE meta_year = '{model_assessment_data_year}'
+  WHERE meta_year = '{params$assessment$data_year}'
   ")
 )
 tictoc::toc()
@@ -123,7 +93,7 @@ land_site_rate_data <- dbGetQuery(
   conn = AWS_ATHENA_CONN_JDBC, glue("
   SELECT *
   FROM other.land_site_rate
-  WHERE year = '{model_assessment_year}'
+  WHERE year = '{params$assessment$year}'
   ")
 )
 
@@ -131,7 +101,7 @@ land_nbhd_rate_data <- dbGetQuery(
   conn = AWS_ATHENA_CONN_JDBC, glue("
   SELECT *
   FROM other.land_nbhd_rate
-  WHERE year = '{model_assessment_year}'
+  WHERE year = '{params$assessment$year}'
   ")
 )
 tictoc::toc()
@@ -171,7 +141,7 @@ recode_column_type <- function(col, col_name, dict = col_type_dict) {
 ## 3.1. Training Data ----------------------------------------------------------
 
 # Clean up the training data. Goal is to get it into a publishable format.
-# Final featurization, filling, etc. is handled via recipes
+# Final featurization, filling, etc. is handled via Tidymodels recipes
 training_data_clean <- training_data %>%
   
   # Recode factor variables using the definitions stored in ccao::vars_dict
@@ -216,7 +186,7 @@ assessment_data_clean <- assessment_data %>%
   # the sale price of properties on the date of assessment. Not the date of an
   # actual sale
   dplyr::mutate(
-    meta_sale_date = as_date(model_assessment_date),
+    meta_sale_date = as_date(params$assessment$date),
     time_interval = interval(ymd("1997-01-01"), ymd(.data$meta_sale_date)),
     time_sale_year = year(meta_sale_date),
     time_sale_week = time_interval %/% weeks(1),
@@ -241,70 +211,63 @@ assessment_data_clean <- assessment_data %>%
 # joining and then link each PIN into an undirected graph. See this SO post
 # for more details on the methodology: 
 # https://stackoverflow.com/questions/68353869/create-group-based-on-fuzzy-criteria
-if (model_generate_complex_id) {
-  complex_id_temp <- assessment_data_clean %>%
-    filter(meta_class %in% c("210", "295")) %>%
-    
-    # Self-join with attributes that must be exactly matching
-    select(
-      meta_pin, meta_card_num, meta_township_code, meta_class,
-      char_bsmt, char_gar1_size, char_attic_fnsh, char_beds,
-      char_rooms, char_bldg_sf, char_yrblt, loc_x_3435, loc_y_3435 
-    ) %>%
-    full_join(
-      eval(.), by = c(
-        "meta_township_code", "meta_class", "char_bsmt",
-        "char_gar1_size", "char_attic_fnsh", "char_beds"
-      )
-    ) %>%
-    
-    # Filter with attributes that can be "fuzzy" matched
-    filter(
-      char_rooms.x >= char_rooms.y - 1,
-      char_rooms.x <= char_rooms.y + 1,
-      char_bldg_sf.x >= char_bldg_sf.y - 25,
-      char_bldg_sf.x <= char_bldg_sf.y + 25,
-      ((char_yrblt.x >= char_yrblt.y - 4 &
-          char_yrblt.x <= char_yrblt.y + 4) |
-         is.na(char_yrblt.x)
-      ),
-      
-      # Units must be within 250 feet of other units
-      ((loc_x_3435.x >= loc_x_3435.y - 250 &
-          loc_x_3435.x <= loc_x_3435.y + 250) |
-         is.na(loc_x_3435.x)
-      ),
-      ((loc_y_3435.x >= loc_y_3435.y - 250 &
-          loc_y_3435.x <= loc_y_3435.y + 250) |
-         is.na(loc_y_3435.x)
-      )
-    ) %>%
-    
-    # Combine PINs into a graph
-    select(meta_pin.x, meta_pin.y) %>%
-    igraph::graph_from_data_frame(directed = FALSE) %>%
-    igraph::components() %>%
-    igraph::membership() %>%
-    
-    # Convert graph to tibble and clean up
-    utils::stack() %>%
-    as_tibble() %>%
-    mutate(ind = as.character(ind)) %>%
-    rename(meta_pin = ind, meta_complex_id = values)
+complex_id_temp <- assessment_data_clean %>%
+  filter(meta_class %in% c("210", "295")) %>%
   
-  # Attach original PIN data and fill any missing with a sequential integer
-  complex_id_data <- assessment_data_clean %>%
-    filter(meta_class %in% c("210", "295")) %>%
-    distinct(meta_pin, meta_township_code, meta_class) %>%
-    left_join(complex_id_temp, by = "meta_pin") %>%
-    arrange(meta_complex_id) %>%
-    mutate(meta_complex_id = ifelse(
-      !is.na(meta_complex_id),
-      meta_complex_id,
-      lag(meta_complex_id) + 1
-    )) %>%
-    write_parquet(paths$input$complex_id$local)
-}
+  # Self-join with attributes that must be exactly matching
+  select(
+    meta_pin, meta_card_num, meta_township_code, meta_class,
+    char_bsmt, char_gar1_size, char_attic_fnsh, char_beds,
+    char_rooms, char_bldg_sf, char_yrblt, loc_x_3435, loc_y_3435 
+  ) %>%
+  full_join(eval(.), by = params$input$complex$match_exact) %>%
+  
+  # Filter with attributes that can be "fuzzy" matched
+  filter(
+    char_rooms.x >= char_rooms.y - params$input$complex$match_fuzzy$rooms,
+    char_rooms.x <= char_rooms.y + params$input$complex$match_fuzzy$rooms,
+    char_bldg_sf.x >= char_bldg_sf.y - params$input$complex$match_fuzzy$bldg_sf,
+    char_bldg_sf.x <= char_bldg_sf.y + params$input$complex$match_fuzzy$bldg_sf,
+    ((char_yrblt.x >= char_yrblt.y - params$input$complex$match_fuzzy$yrblt &
+      char_yrblt.x <= char_yrblt.y + params$input$complex$match_fuzzy$yrblt) |
+      is.na(char_yrblt.x)
+    ),
+    
+    # Units must be within 250 feet of other units
+    ((loc_x_3435.x >= loc_x_3435.y - params$input$complex$match_fuzzy$dist_ft &
+      loc_x_3435.x <= loc_x_3435.y + params$input$complex$match_fuzzy$dist_ft) |
+      is.na(loc_x_3435.x)
+    ),
+    ((loc_y_3435.x >= loc_y_3435.y - params$input$complex$match_fuzzy$dist_ft &
+      loc_y_3435.x <= loc_y_3435.y + params$input$complex$match_fuzzy$dist_ft) |
+      is.na(loc_y_3435.x)
+    )
+  ) %>%
+  
+  # Combine PINs into a graph
+  select(meta_pin.x, meta_pin.y) %>%
+  igraph::graph_from_data_frame(directed = FALSE) %>%
+  igraph::components() %>%
+  igraph::membership() %>%
+  
+  # Convert graph to tibble and clean up
+  utils::stack() %>%
+  as_tibble() %>%
+  mutate(ind = as.character(ind)) %>%
+  rename(meta_pin = ind, meta_complex_id = values)
+
+# Attach original PIN data and fill any missing with a sequential integer
+complex_id_data <- assessment_data_clean %>%
+  filter(meta_class %in% c("210", "295")) %>%
+  distinct(meta_pin, meta_township_code, meta_class) %>%
+  left_join(complex_id_temp, by = "meta_pin") %>%
+  arrange(meta_complex_id) %>%
+  mutate(meta_complex_id = ifelse(
+    !is.na(meta_complex_id),
+    meta_complex_id,
+    lag(meta_complex_id) + 1
+  )) %>%
+  write_parquet(paths$input$complex_id$local)
 
 
 ## 3.4. Land Rates -------------------------------------------------------------
