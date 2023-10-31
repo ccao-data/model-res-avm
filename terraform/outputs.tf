a1db4eabddc29c76cb665024b64e96473de171cb
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2023-10-31 19:24:36 +0000

    Refactor deploy.yaml to move Terraform setup into dedicated setup-terraform action

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "batch_job_queue_arn" {
   description = "ARN of the Batch job queue"
   value       = aws_batch_job_queue.main.arn
 }
+
+output "batch_job_name" {
+  description = "Name of the Batch job"
+  value       = var.batch_job_name
+}
