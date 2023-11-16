26c61bbb2c171a204ba6ae263f1ee07df5fbc18d
Jean Cochrane <jean@jeancochrane.com>
2023-11-16 16:00:20 -0600

    Clean up docstrings and test against 2023-11-14-frosty-jacob model

--- R/delete_current_year_model_runs.R
+++ R/delete_current_year_model_runs.R
@@ -1,7 +1,7 @@
 # Script to delete a list of model runs by ID from AWS.
 #
-# Assumes that model runs are restricted to the current upcoming
-# assessment cycle year.
+# Assumes that model runs are restricted to the current assessment cycle, where
+# each assessment cycle starts in April.
 
 library(here)
 library(magrittr)
@@ -13,7 +13,7 @@ current_year <- current_date %>% format("%Y")
 
 # The following heuristic determines the current upcoming assessment cycle year:
 #
-#   * From March to December (post assessment), `year` = next year
+#   * From April to December (post assessment), `year` = next year
 #   * From January to March (during assessment), `year` = current year
 year <- if (current_month < "03") current_year else as.character(as.numeric(current_year) + 1)
 
@@ -21,4 +21,4 @@ run_ids <- commandArgs(trailingOnly = TRUE)
 
 sprintf("Deleting run IDs for year %s: %s", year, run_ids)
 
-# run_ids %>% sapply(model_delete_run, year = year)
+run_ids %>% sapply(model_delete_run, year = year)
