706cf5bb9799a6335d7d7fd3336c1718f3ffb76b
Dan Snow <daniel.snow@cookcountyil.gov>
2023-12-13 21:52:40 +0000

    Change upload_year param to working_year

--- pipeline/06-upload.R
+++ pipeline/06-upload.R
@@ -28,7 +28,7 @@ if (upload_enable) {
   # Initialize a dictionary of paths AND S3 URIs specific to the run ID and year
   paths <- model_file_dict(
     run_id = run_id,
-    year = params$assessment$upload_year
+    year = params$assessment$working_year
   )
 
 
@@ -124,7 +124,7 @@ if (upload_enable) {
   # reduce file size and improve query performance we partition them by year,
   # run ID, and township
   read_parquet(paths$output$assessment_card$local) %>%
-    mutate(run_id = run_id, year = params$assessment$upload_year) %>%
+    mutate(run_id = run_id, year = params$assessment$working_year) %>%
     group_by(year, run_id, township_code) %>%
     arrow::write_dataset(
       path = paths$output$assessment_card$s3,
@@ -133,7 +133,7 @@ if (upload_enable) {
       compression = "snappy"
     )
   read_parquet(paths$output$assessment_pin$local) %>%
-    mutate(run_id = run_id, year = params$assessment$upload_year) %>%
+    mutate(run_id = run_id, year = params$assessment$working_year) %>%
     group_by(year, run_id, township_code) %>%
     arrow::write_dataset(
       path = paths$output$assessment_pin$s3,
@@ -177,7 +177,7 @@ if (upload_enable) {
   if (shap_enable) {
     message("Uploading SHAP values")
     read_parquet(paths$output$shap$local) %>%
-      mutate(run_id = run_id, year = params$assessment$upload_year) %>%
+      mutate(run_id = run_id, year = params$assessment$working_year) %>%
       group_by(year, run_id, township_code) %>%
       arrow::write_dataset(
         path = paths$output$shap$s3,
