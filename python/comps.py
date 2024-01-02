5beefd56dc5405c17871f63fa8f444d7e5c3b501
Jean Cochrane <jean@jeancochrane.com>
2024-01-02 11:51:03 -0600

    Swap arg order in _get_similarity_matrix to confirm numba error message

--- python/comps.py
+++ python/comps.py
@@ -13,7 +13,7 @@ def get_comps(leaf_node_df, weights, n=20):
     # of numba acceleration
     leaf_node_matrix = leaf_node_df.values
     weights_arr = np.asarray(weights, dtype=np.float64)
-    comps = _get_similarity_matrix(leaf_node_matrix, weights_arr)
+    comps = _get_similarity_matrix(weights_arr, leaf_node_matrix)
     # Extract the top n matches from each row of the comp matrix
     # by sorting the columns for each row, selecting the last n indices, and
     # reversing the order
@@ -26,7 +26,7 @@ def get_comps(leaf_node_df, weights, n=20):
 
 
 @nb.njit(fastmath=True)
-def _get_similarity_matrix(leaf_node_matrix, weights):
+def _get_similarity_matrix(weights, leaf_node_matrix):
     """Helper function that takes a matrix of leaf node assignments for each
     observation in a tree model and an array of weights for each tree, and
     returns a matrix scoring the similarity every observation to every other
