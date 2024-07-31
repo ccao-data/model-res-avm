f4c0679a0b69be65d0b604ea864d6682d5aa2e84
Damonamajor <damon.major@cookcountyil.gov>
2024-07-31 20:44:51 +0000

    Add unique components to code_to_review

--- analyses/code_to_review.R
+++ analyses/code_to_review.R
@@ -1,19 +1,76 @@
+## Create paths for downloads
+
+base_paths <- model_file_dict(params$run_id, params$run_id_year)
+comp_paths <-
+  model_file_dict(params$comparison_run_id, params$comparison_run_id_year)
+
+analyses_paths <- list(
+  output = list(
+    list(
+      s3 = base_paths$output$assessment_card$s3,
+      key = "assessment_card"
+    ),
+    list(
+      s3 = base_paths$output$assessment_pin$s3,
+      key = "assessment_pin"
+    ),
+    list(
+      s3 = base_paths$output$metadata$s3,
+      key = "metadata"
+    ),
+    list(
+      s3 = base_paths$output$performance_test$s3,
+      key = "performance"
+    ),
+    list(
+      s3 = base_paths$output$shap$s3,
+      key = "shap"
+    )
+  )
+)
+
+comp_paths <- list(
+  output = list(
+    list(
+      s3 = comp_paths$output$assessment_card$s3,
+      key = "assessment_card"
+    ),
+    list(
+      s3 = comp_paths$output$assessment_pin$s3,
+      key = "assessment_pin"
+    ),
+    list(
+      s3 = comp_paths$output$metadata$s3,
+      key = "metadata"
+    ),
+    list(
+      s3 = comp_paths$output$performance_test$s3,
+      key = "performance"
+    ),
+    list(
+      s3 = comp_paths$output$shap$s3,
+      key = "shap"
+    )
+  )
+)
+
+## Create base datasets
+
 target_feature_value <- params$added_feature
 target_feature_shap <- params$added_feature_shap
 nbhd <- ccao::nbhd_shp
 
 # Selecting and joining relevant data
 card_individual <- shap %>%
-  select(meta_pin, meta_card_num, pred_card_shap_baseline_fmv,
+  select(meta_pin, meta_nbhd_code, meta_card_num, pred_card_shap_baseline_fmv,
          {{ target_feature_value }}) %>%
   rename(!!target_feature_shap := !!target_feature_value) %>%
   inner_join(assessment_card, by = c("meta_pin", "meta_card_num")) %>%
-  inner_join(
-    assessment_data %>%
-      select(meta_pin, meta_card_num, meta_nbhd_code,
-             loc_longitude, loc_latitude, meta_township_name),
-    by = c("meta_pin", "meta_card_num")
+  mutate(
+    relative_shap =
+      round(!!sym({{target_feature_shap}}) / pred_card_initial_fmv, 2)
   )
+
 
 
 # Summarizing data by neighborhood code
@@ -23,9 +80,7 @@ card_nbhd <- card_individual %>%
     avg_target_feature_shap =
       mean(!!sym({{target_feature_shap}}), na.rm = TRUE),
     avg_target_feature_shap_abs =
-      mean(abs(!!sym({{target_feature_shap}})), na.rm = TRUE),
-    avg_pred_card_shap_baseline_fmv =
-      mean(pred_card_shap_baseline_fmv, na.rm = TRUE)
+      mean(abs(!!sym({{target_feature_shap}})), na.rm = TRUE)
   ) %>%
   ungroup()
 
@@ -49,18 +104,19 @@ pin_individual <- assessment_pin %>%
     diff_pred_pin_final_fmv = round(((
                                       pred_pin_final_fmv_new -
                                         pred_pin_final_fmv_comp) /
-      pred_pin_final_fmv_comp) * 100, 2),
+      pred_pin_final_fmv_comp), 4),
     pred_pin_final_fmv_new = dollar(pred_pin_final_fmv_new),
     pred_pin_final_fmv_comp = dollar(pred_pin_final_fmv_comp),
     diff_pred_pin_initial_fmv = round(((
                                         pred_pin_initial_fmv_new -
                                           pred_pin_initial_fmv_comp) /
-      pred_pin_initial_fmv_comp) * 100, 2),
+      pred_pin_initial_fmv_comp), 4),
     pred_pin_initial_fmv_new = dollar(pred_pin_initial_fmv_new),
     pred_pin_initial_fmv_comp = dollar(pred_pin_initial_fmv_comp)
   ) %>%
   inner_join(
     assessment_data %>%
+      distinct(meta_pin, .keep_all = TRUE) %>%
       select(meta_pin, meta_nbhd_code, loc_longitude,
              loc_latitude, meta_township_name, {{ target_feature_value }}),
     by = "meta_pin"
@@ -82,5 +138,40 @@ pin_nbhd <- pin_individual %>%
     by = c("meta_nbhd_code" = "town_nbhd")
   )
 
+leaflet_data <- card_individual %>%
+  select(meta_pin, {{target_feature_shap}}) %>%
+  right_join(pin_individual,
+             by = c("meta_pin" = "meta_pin"))
 
 
+## Example of violin plots
+
+pin_individual %>%
+  filter(
+    diff_pred_pin_initial_fmv >=
+      ## Keeping all values creates really weird charts
+      quantile(diff_pred_pin_initial_fmv, 0.025, na.rm = TRUE) &
+      diff_pred_pin_initial_fmv <=
+        quantile(diff_pred_pin_initial_fmv, 0.975, na.rm = TRUE)
+  ) %>%
+  mutate(
+    mean_value = mean(diff_pred_pin_initial_fmv, na.rm = TRUE),
+    median_value = median(diff_pred_pin_initial_fmv, na.rm = TRUE)
+  ) %>%
+  ggplot(aes(x = 1, y = diff_pred_pin_initial_fmv)) +
+  geom_violin(fill = "blue", color = "black", alpha = 0.7) +
+  geom_hline(aes(yintercept = mean_value, color = "Mean"),
+             linetype = "dashed", linewidth = 1, show.legend = TRUE) +
+  geom_hline(aes(yintercept = median_value, color = "Median"),
+             linetype = "dashed", linewidth = 1, show.legend = TRUE) +
+  scale_color_manual(
+    name = "Statistics",
+    values = c(Mean = "red", Median = "green"),
+    labels = c("Mean", "Median")
+  ) +
+  scale_y_continuous(labels = scales::percent) + # Format y-axis as percentages
+  labs(
+    x = "Distribution",
+    y = "Change in FMV"
+  ) +
+  theme_minimal()
