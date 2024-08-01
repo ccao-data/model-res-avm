fd79b847b835ad1b032507b2985a26d1cbd2b3e5
Damonamajor <damon.major@cookcountyil.gov>
2024-08-01 18:50:20 +0000

    Add append ingest scripts, Add link to source

--- analyses/code_to_review.R
+++ analyses/code_to_review.R
@@ -1,29 +1,47 @@
-## Create paths for downloads
-
 base_paths <- model_file_dict(params$run_id, params$run_id_year)
 comp_paths <-
   model_file_dict(params$comparison_run_id, params$comparison_run_id_year)
 run_id <- params$run_id
 comparison_run_id <- params$comparison_run_id
 
-# Define the list of outputs
-output_list <- list(
-  assessment_card = base_paths$output$assessment_card$s3,
-  assessment_pin = base_paths$output$assessment_pin$s3,
-  metadata = base_paths$output$metadata$s3,
-  performance_test = base_paths$output$performance_test$s3,
-  shap = base_paths$output$shap$s3
-)
 
-# Create the analyses_paths dynamically
 analyses_paths <- list(
-  output = map(output_list, ~list(s3 = .x, key = gsub(".*\\$", "", deparse(substitute(.x)))))
+  output = list(
+    list(
+      s3 = base_paths$output$assessment_card$s3,
+      key = "assessment_card"
+    ),
+    list(
+      s3 = base_paths$output$assessment_pin$s3,
+      key = "assessment_pin"
+    ),
+    list(
+      s3 = base_paths$output$metadata$s3,
+      key = "metadata"
+    ),
+    list(
+      s3 = base_paths$output$performance_test$s3,
+      key = "performance_test"
+    ),
+    list(
+      s3 = base_paths$output$shap$s3,
+      key = "shap"
+    )
+  )
 )
 
+
+
+source("analyses/Ingest_script.qmd")
+
+data <- model_fetch_run_subset(params$run_id, params$run_id_year, analyses_paths, TRUE)
+
+list2env(data, envir = .GlobalEnv)
 
 target_feature_value <- params$added_feature
 target_feature_shap <- params$added_feature_shap
 nbhd <- ccao::nbhd_shp
+
 
 
 # Selecting and joining relevant data
