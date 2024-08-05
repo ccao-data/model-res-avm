1993c86a67fcaf3a422727c95b63e006e77446f2
Damonamajor <damon.major@cookcountyil.gov>
2024-08-05 17:40:51 +0000

    Switch to simple loop

--- Data_ingest.R
+++ Data_ingest.R
@@ -6,7 +6,6 @@ comparison_paths <- model_file_dict(
 run_id <- params$run_id
 comparison_run_id <- params$comparison_run_id
 
-
 analyses_paths <- list(
   output = list(
     list(
@@ -80,17 +79,18 @@ rm(data_comparison)
 
 all_vars <- ls()
 
-rename_var <- function(var_name) {
-  rename_conditionally <- function(suffix, new_suffix) {
-    if (exists(var_name) && is.data.frame(get(var_name)) &&
-      grepl(paste0("_", suffix, "$"), var_name)) {
+rename_var <- function(var_name, suffix, new_suffix) {
+  if (exists(var_name) && is.data.frame(get(var_name))) {
+    if (grepl(paste0("_", suffix, "$"), var_name)) {
       new_name <- sub(paste0("_", suffix, "$"), new_suffix, var_name)
       assign(new_name, get(var_name), envir = .GlobalEnv)
       rm(list = var_name, envir = .GlobalEnv)
     }
   }
-  rename_conditionally(params$run_id, "_new")
-  rename_conditionally(params$comp_run_id, "_comparison")
 }
 
-lapply(all_vars, rename_var)
+# Iterate over all variables and rename if necessary
+for (var_name in all_vars) {
+  rename_var(var_name, params$run_id, "_new")
+  rename_var(var_name, params$comparison_run_id, "_comparison")
+}
