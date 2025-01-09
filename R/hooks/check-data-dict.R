7994e4fd82ac59c1333a8696e5a065334f513112
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2025-01-09 19:58:40 +0000

    Better error msg and dependencies in check-data-dict.R pre-commit hook

--- R/hooks/check-data-dict.R
+++ R/hooks/check-data-dict.R
@@ -1,9 +1,30 @@
 #!/usr/bin/env Rscript
 # Script to check that the data dictionary file is up to date with the
 # latest feature set
-library(readr)
 library(yaml)
 
-params <- read_yaml("params.yaml")
-data_dict <- read_csv("docs/data-dict.csv", show_col_types = FALSE)
-stopifnot(setequal(data_dict$variable_name, params$model$predictor$all))
+params_filename <- "params.yaml"
+data_dict_filename <- "docs/data-dict.csv"
+
+params <- read_yaml(params_filename)
+data_dict <- read.csv(data_dict_filename)
+
+symmetric_diff <- c(
+  setdiff(data_dict$variable_name, params$model$predictor$all),
+  setdiff(params$model$predictor$all, data_dict$variable_name)
+)
+symmetric_diff_len <- length(symmetric_diff)
+
+if (symmetric_diff_len > 0) {
+  err_msg_prefix <- ifelse(symmetric_diff_len == 1, "Param is", "Params are")
+  err_msg <- paste0(
+    err_msg_prefix,
+    " not present in both ",
+    params_filename,
+    " and ",
+    data_dict_filename,
+    ": ",
+    paste(symmetric_diff, collapse = ", ")
+  )
+  stop(err_msg)
+}
