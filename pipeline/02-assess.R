c4cfe54d2d20373de06f8ac6ec8e8be3e7ecc70d
Dan Snow <31494343+dfsnow@users.noreply.github.com>
2025-02-14 14:21:30 -0600

    Fix missing sales validation reasons in DR workbooks (#348)
    * Update outlier reason fields in DR workbooks
    
    * Fix missing sales val reasons in DR workbooks
    
    * Fix linting issue

--- pipeline/02-assess.R
+++ pipeline/02-assess.R
@@ -382,12 +382,13 @@ sales_data_ratio_study <- sales_data %>%
 sales_data_two_most_recent <- sales_data %>%
   distinct(
     meta_pin, meta_year,
-    meta_sale_price, meta_sale_date, meta_sale_document_num,
+    meta_sale_price, meta_sale_date, meta_sale_document_num, sv_is_outlier,
     sv_outlier_reason1, sv_outlier_reason2, sv_outlier_reason3
   ) %>%
   # Include outliers, since these data are used for desk review and
   # not for modeling
   rename(
+    meta_sale_is_outlier = sv_is_outlier,
     meta_sale_outlier_reason1 = sv_outlier_reason1,
     meta_sale_outlier_reason2 = sv_outlier_reason2,
     meta_sale_outlier_reason3 = sv_outlier_reason3
@@ -402,6 +403,7 @@ sales_data_two_most_recent <- sales_data %>%
       meta_sale_date,
       meta_sale_price,
       meta_sale_document_num,
+      meta_sale_is_outlier,
       meta_sale_outlier_reason1,
       meta_sale_outlier_reason2,
       meta_sale_outlier_reason3
