002bfa6970555a8d266fed39b9a8a5a9f9302647
Dan Snow <daniel.snow@cookcountyil.gov>
2025-02-24 18:51:41 +0000

    Remove unnecessary ungroup

--- pipeline/07-export.R
+++ pipeline/07-export.R
@@ -397,8 +397,7 @@ if (comp_enable) {
 
   # Query and filter training data to use as a comp detail view
   training_data <- read_parquet(paths$input$training$local) %>%
-    filter(!ind_pin_is_multicard, !sv_is_outlier) %>%
-    ungroup()
+    filter(!ind_pin_is_multicard, !sv_is_outlier)
 } else {
   # Add NA columns for comps so that assessment_pin_merged has the same
   # shape in both conditional branches
