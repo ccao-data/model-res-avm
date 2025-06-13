ae0c77924f900498ed1d28fe5126c185cb4acbd8
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2025-06-13 22:11:44 +0000

    Test weighting all trees that reduce error equally in comps pipeline

--- R/helpers.R
+++ R/helpers.R
@@ -222,7 +222,10 @@ extract_tree_weights <- function(model,
 
   # Take proportion of diff in errors over total diff in
   # errors from all trees
-  weights <- diff_in_errors / rowSums(diff_in_errors)
+  # Temporarily turn off weighting by setting weights to 1 if the tree
+  # reduces error at all, and 0 otherwise
+  weights <- ifelse(diff_in_errors > 0, 1, 0)
+  weights <- weights / rowSums(weights)
   weights[is.nan(weights)] <- 0
 
   return(weights)
