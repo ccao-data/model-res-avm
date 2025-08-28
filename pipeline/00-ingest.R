fde8175b04dda83c121729c9fae9888fa85ba866
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2025-08-28 11:19:01 -0500

    Update 00-ingest.R

--- pipeline/00-ingest.R
+++ pipeline/00-ingest.R
@@ -522,14 +522,14 @@ message("Creating townhome complex identifiers")
 # https://stackoverflow.com/questions/68353869/create-group-based-on-fuzzy-criteria # nolint
 complex_id_temp <- assessment_data_clean %>%
   filter(meta_class %in% c("210", "295")) %>%
-  mutate(char_gar1_size = as.character(char_gar1_size)) #
+  mutate(char_gar1_size = as.character(char_gar1_size)) %>%
 # Self-join with attributes that must be exactly matching
 select(
   meta_pin, meta_card_num, meta_township_code, meta_class,
   all_of(params$input$complex$match_exact),
   any_of(paste0("char_", names(params$input$complex$match_fuzzy))),
   loc_x_3435, loc_y_3435
-) %>%
+  ) %>%
   full_join(
     eval(.),
     by = params$input$complex$match_exact,
