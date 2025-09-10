4db6b7b78fce5f0e19de33aef2eb81946c8f6459
Damonamajor <damon.major@cookcountyil.gov>
2025-09-10 17:43:06 +0000

    increase square footage

--- pipeline/00-ingest.R
+++ pipeline/00-ingest.R
@@ -20,6 +20,7 @@ suppressPackageStartupMessages({
 })
 
 # Adds arrow support to speed up ingest process.
+noctua_options(unload = TRUE)
 
 # Establish Athena connection
 AWS_ATHENA_CONN_NOCTUA <- dbConnect(
@@ -520,16 +521,8 @@ message("Creating townhome complex identifiers")
 # joining and then link each PIN into an undirected graph. See this SO post
 # for more details on the methodology:
 # https://stackoverflow.com/questions/68353869/create-group-based-on-fuzzy-criteria # nolint
-garage_crosswalk <- tibble::tibble(
-  char_gar1_size = as.character(1:8),
-  garage_num = c(1, 1.5, 2, 2.5, 3, 3.5, 0, 4)
-)
-
 complex_id_temp <- assessment_data_clean %>%
   filter(meta_class %in% c("210", "295")) %>%
-  left_join(garage_crosswalk, by = "char_gar1_size") %>%
-  mutate(char_gar1_size = garage_num) %>%
-  select(-garage_num) %>%
   # Self-join with attributes that must be exactly matching
   select(
     meta_pin, meta_card_num, meta_township_code, meta_class,
@@ -544,16 +537,12 @@ complex_id_temp <- assessment_data_clean %>%
     relationship = "many-to-many"
   ) %>%
   # Filter with attributes that can be "fuzzy" matched
-  # nolint start
   filter(
     char_rooms.x >= char_rooms.y - params$input$complex$match_fuzzy$rooms,
     char_rooms.x <= char_rooms.y + params$input$complex$match_fuzzy$rooms,
-    char_beds.x >= char_beds.y - params$input$complex$match_fuzzy$beds,
-    char_beds.x <= char_beds.y + params$input$complex$match_fuzzy$beds,
-    char_gar1_size.x >= char_gar1_size.y - params$input$complex$match_fuzzy$gar1_size,
-    char_gar1_size.x <= char_gar1_size.y + params$input$complex$match_fuzzy$gar1_size,
     char_bldg_sf.x >= char_bldg_sf.y - params$input$complex$match_fuzzy$bldg_sf,
     char_bldg_sf.x <= char_bldg_sf.y + params$input$complex$match_fuzzy$bldg_sf,
+    # nolint start
     (char_yrblt.x >= char_yrblt.y - params$input$complex$match_fuzzy$yrblt &
       char_yrblt.x <= char_yrblt.y + params$input$complex$match_fuzzy$yrblt) |
       is.na(char_yrblt.x),
