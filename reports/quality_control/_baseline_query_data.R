69abb3b3fe5e9b5d8c3919952b0e0209e9b195cc
Damonamajor <damon.major@cookcountyil.gov>
2025-12-15 21:41:05 +0000

    Begining of Jean edits

--- reports/quality_control/_baseline_query_data.R
+++ reports/quality_control/_baseline_query_data.R
@@ -1,11 +1,82 @@
 source("_utils.R")
 
+noctua_options(cache_size = 10, unload = TRUE)
+conn <- dbConnect(noctua::athena(), rstudio_conn_tab = FALSE)
+
+# Model metadata
+baseline_metadata <- dbGetQuery(
+  conn,
+  # This query should only ever return one row, but limit the results to 1
+  # just to be defensive
+  glue(
+    "
+    select
+      dvc_md5_assessment_data,
+      dvc_md5_training_data,
+      model_predictor_all_name,
+      model_predictor_categorical_name,
+      assessment_year
+    from model.metadata
+    where run_id = '{params$baseline_run_id}'
+    limit 1
+    "
+  )
+)
+
+baseline_year <- baseline_metadata$assessment_year
+
+model_predictor_all_name <-
+  baseline_metadata$model_predictor_all_name %>%
+  unlist()
+
+model_predictor_categorical_name <-
+  baseline_metadata$model_predictor_categorical_name %>%
+  unlist()
+
+dvc_md5_assessment_data <- baseline_metadata$dvc_md5_assessment_data
+dvc_md5_training_data <- baseline_metadata$dvc_md5_training_data
+
+
+# Model metadata
+comp_metadata <- dbGetQuery(
+  conn,
+  # This query should only ever return one row, but limit the results to 1
+  # just to be defensive
+  glue(
+    "
+    select
+      dvc_md5_assessment_data,
+      dvc_md5_training_data,
+      model_predictor_all_name,
+      assessment_year,
+      model_predictor_categorical_name
+    from model.metadata
+    where run_id = '{params$comp_run_id}'
+    limit 1
+    "
+  )
+)
+
+assessment_year_comp <- comp_metadata$assessment_year
+
+dvc_md5_assessment_data_comp <- comp_metadata$dvc_md5_assessment_data
+
+# Split categorical and continuous predictors since we need to plot them
+# differently (e.g. count vs. bin histograms, respectively)
+categorical_preds <- model_predictor_categorical_name
+continuous_preds <- setdiff(
+  model_predictor_all_name,
+  model_predictor_categorical_name
+)
+
+categorical_shaps <- paste0(categorical_preds, "_shap")
+continuous_shaps <- paste0(continuous_preds, "_shap")
 
 base_dvc_url <- "s3://ccao-data-dvc-us-east-1"
 base_model_results_url <- "s3://ccao-model-results-us-east-1"
 
 # Assessment set chars
-baseline_assessment_data <- open_dataset(
+baseline_chars <- open_dataset(
   paste0(
     glue("{base_dvc_url}/files/md5/"),
     substr(dvc_md5_assessment_data, 1, 2), "/",
@@ -37,6 +108,26 @@ comp_chars <- open_dataset(
   ) %>%
   collect()
 
+# Assessment set predictions
+baseline_assess_card <- open_dataset(
+  paste0(
+    glue("{base_model_results_url}/assessment_card/"),
+    glue("year={params$baseline_year}/"),
+    glue("run_id={params$baseline_run_id}")
+  )
+) %>%
+  collect()
+
+# Grab PIN predictions so we can get the final rounded value
+baseline_assess_pin <- open_dataset(
+  paste0(
+    glue("{base_model_results_url}/assessment_pin/"),
+    glue("year={params$baseline_year}/"),
+    glue("run_id={params$baseline_run_id}")
+  )
+) %>%
+  collect()
+
 # SHAPs
 baseline_shaps <- open_dataset(
   paste0(
@@ -47,7 +138,7 @@ baseline_shaps <- open_dataset(
 ) %>%
   collect() %>%
   left_join(
-    baseline_assessment_data,
+    baseline_chars,
     by = c("meta_pin", "meta_card_num"),
     suffix = c("_shap", "")
   ) %>%
@@ -73,4 +164,5 @@ baseline_training_data <- open_dataset(
   ) %>%
   collect()
 
-gc()
+# Clean up large objects that we no longer need
+rm(baseline_assess_card, baseline_assess_pin)
