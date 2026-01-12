bbc7bebc07daff3945a1df3319af44b1e57c17f6
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-01-12 15:48:01 -0600

    Consolidate and formalize model report for feature analysis (#401)
    This merges two existing EI issues to create a consolidated QC testing
    framework to run once each year upon final upload of data. It uses 4
    reports, 3 derived from
    [this](https://github.com/ccao-data/enterprise-intelligence/pull/207)
    and 1 from
    [this](https://github.com/ccao-data/enterprise-intelligence/pull/218).
    
    The feature missingness creates a series of thresholds to test if values
    change a significant amount. For example, all acs values should not
    match, but the percentages should not change by more than 10%.
    
    I also refactored the data_changes script from the previous EI issue so
    it runs in about half the time.
    
    Because the file takes a long time to run, and it has limited impact
    once data has been finalized. It should be run manually, not integrated
    in the model pipeline.
    
    In total the run time appears to be about 2 hours for the actual script,
    and for some reason, the rendering takes approximately 1 hour.
    
    output is too large to upload, so it is stored here:
    `O:\CCAODATA\tmp\model_features.html`
    
    ---------
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

---
+++ reports/model_features/_baseline_query_data.R
@@ -0,0 +1,180 @@
+noctua_options(cache_size = 10, unload = TRUE)
+conn <- dbConnect(noctua::athena(), rstudio_conn_tab = FALSE)
+
+base_dvc_url <- "s3://ccao-data-dvc-us-east-1"
+base_model_results_url <- "s3://ccao-model-results-us-east-1"
+
+# Model metadata
+if (!exists("baseline_metadata")) {
+  baseline_metadata <- dbGetQuery(
+    conn,
+    # This query should only ever return one row, but limit the results to 1
+    # just to be defensive
+    glue(
+      "
+    select
+      dvc_md5_assessment_data,
+      dvc_md5_training_data,
+      model_predictor_all_name,
+      model_predictor_categorical_name,
+      assessment_year
+    from model.metadata
+    where run_id = '{params$baseline_run_id}'
+    limit 1
+    "
+    )
+  )
+}
+
+
+if (!exists("model_predictor_all_name")) {
+  model_predictor_all_name <-
+    baseline_metadata$model_predictor_all_name %>%
+    unlist()
+}
+
+if (!exists("model_predictor_categorical_name")) {
+  model_predictor_categorical_name <-
+    baseline_metadata$model_predictor_categorical_name %>%
+    unlist()
+}
+
+if (!exists("dvc_md5_assessment_data")) {
+  dvc_md5_assessment_data <- baseline_metadata$dvc_md5_assessment_data
+}
+
+if (!exists("dvc_md5_training_data")) {
+  dvc_md5_training_data <- baseline_metadata$dvc_md5_training_data
+}
+
+# Model metadata
+if (!exists("comp_metadata")) {
+  comp_metadata <- dbGetQuery(
+    conn,
+    # This query should only ever return one row, but limit the results to 1
+    # just to be defensive
+    glue(
+      "
+    select
+      dvc_md5_assessment_data,
+      dvc_md5_training_data,
+      model_predictor_all_name,
+      assessment_year,
+      model_predictor_categorical_name
+    from model.metadata
+    where run_id = '{params$comp_run_id}'
+    limit 1
+    "
+    )
+  )
+}
+
+if (!exists("assessment_year_comp")) {
+  assessment_year_comp <- comp_metadata$assessment_year
+}
+
+if (!exists("dvc_md5_assessment_data_comp")) {
+  dvc_md5_assessment_data_comp <- comp_metadata$dvc_md5_assessment_data
+}
+
+if (!exists("comp_chars")) {
+  comp_chars <- open_dataset(
+    paste0(
+      glue("{base_dvc_url}/files/md5/"),
+      substr(dvc_md5_assessment_data_comp, 1, 2), "/",
+      substr(dvc_md5_assessment_data_comp, 3, 32)
+    )
+  ) %>%
+    select(
+      meta_pin,
+      meta_card_num,
+      meta_year,
+      meta_class,
+      any_of(model_predictor_all_name)
+    ) %>%
+    collect()
+}
+
+if (!exists("baseline_year")) {
+  baseline_year <- baseline_metadata$assessment_year
+}
+
+# Split categorical and continuous predictors since we need to plot them
+# differently (e.g. count vs. bin histograms, respectively)
+if (!exists("categorical_preds")) {
+  categorical_preds <- model_predictor_categorical_name
+}
+
+if (!exists("continuous_preds")) {
+  continuous_preds <- setdiff(
+    model_predictor_all_name,
+    model_predictor_categorical_name
+  )
+}
+
+if (!exists("categorical_shaps")) {
+  categorical_shaps <- paste0(categorical_preds, "_shap")
+}
+
+if (!exists("continuous_shaps")) {
+  continuous_shaps <- paste0(continuous_preds, "_shap")
+}
+
+# Assessment set chars
+if (!exists("baseline_assessment_data")) {
+  baseline_assessment_data <- open_dataset(
+    paste0(
+      glue("{base_dvc_url}/files/md5/"),
+      substr(dvc_md5_assessment_data, 1, 2), "/",
+      substr(dvc_md5_assessment_data, 3, 32)
+    )
+  ) %>%
+    select(
+      meta_pin,
+      meta_card_num,
+      meta_year,
+      meta_class,
+      all_of(model_predictor_all_name)
+    ) %>%
+    collect()
+}
+
+# SHAPs
+if (!exists("baseline_shaps")) {
+  baseline_shaps <- open_dataset(
+    paste0(
+      glue("{base_model_results_url}/shap/"),
+      glue("year={baseline_year}/"),
+      glue("run_id={params$baseline_run_id}")
+    )
+  ) %>%
+    collect() %>%
+    left_join(
+      baseline_assessment_data,
+      by = c("meta_pin", "meta_card_num"),
+      suffix = c("_shap", "")
+    ) %>%
+    # This column isn't a real SHAP, it's just an artifact of the join
+    select(-meta_year_shap)
+}
+
+# Training data
+if (!exists("baseline_training_data")) {
+  baseline_training_data <- open_dataset(
+    paste0(
+      glue("{base_dvc_url}/files/md5/"),
+      substr(dvc_md5_training_data, 1, 2), "/",
+      substr(dvc_md5_training_data, 3, 32)
+    )
+  ) %>%
+    select(
+      meta_pin,
+      meta_card_num,
+      meta_year,
+      meta_sale_price,
+      meta_sale_date,
+      meta_class,
+      all_of(model_predictor_all_name)
+    ) %>%
+    collect()
+}
