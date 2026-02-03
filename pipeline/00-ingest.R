a2d6ec68eccbd8651ac9c53b9f0c05c14f68d208
wagnerlmichael <93889413+wagnerlmichael@users.noreply.github.com>
2026-02-03 15:22:30 -0600

    Rework outlier section of performance report to accommodate new spec and simplify (#427)
    This PR reworks the performance report to work with out [new sales val
    data model
    additions](https://github.com/ccao-data/data-architecture/pull/977) and
    add two things to the report:
    - outlier numbers (raw and proportion) per year
    - outlier proportion maps per nbhd incorporated from our geography group
    testing
    
    It also reorganizes the outlier reasons that are displayed
    
    ---------
    
    Co-authored-by: Jean Cochrane <jean@jeancochrane.com>
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- pipeline/00-ingest.R
+++ pipeline/00-ingest.R
@@ -85,7 +85,39 @@ message("Pulling data from Athena")
 
 # Pull the training data, which contains actual sales + attached characteristics
 # from the residential input view. Earlier years are included to help calculate
-# lagged features
+# lagged features.
+#
+# Many of these columns come from our sales validation workflow. Some notes
+# about these columns:
+#
+# - `sv_is_outlier` combines our algorithmic sales validation flags with
+#   findings from human review to produce a final boolean outlier decision.
+#   It is never null, even when a sale has not been flagged or reviewed.
+#
+# - `sv_outlier_reason` is a verbose explanation of the decision behind
+#   `sv_is_outlier`. If a sale has not been flagged or reviewed, it will be
+#   null.
+#
+# - `sv_outlier_reason{N}`, where 1 <= N <= 3, are three columns that we query
+#   directly from the output of our algorithmic sales validation pipeline,
+#   recording the reasons why the pipeline flagged the sale as an outlier.
+#   The names of these columns are somewhat confusing given their similarity to
+#   the `sv_outlier_reason` column, but they are important for backwards
+#   compatibility. Since `sv_is_outlier` factors in findings from reviewers in
+#   addition to this algorithmic pipeline, this field may not match up
+#   intuitively with the boolean decision in `sv_is_outlier`.
+#
+# - `sv_review_json` is a JSON string storing the raw findings from sale review.
+#   Its schema is not guaranteed to have a consistent structure, so it's not
+#   useful for serious analysis or programming tasks. However, since the field
+#   is intended to act only as an archival record of the exact state of the
+#   review findings  at the point in time of a given model run, we think it is
+#   preferable for the field to have a simple-but-maintainable format instead of
+#   a sophisticated-but-complex format. (This kind of archival record is
+#   important because it is theoretically possible that a sale may get
+#   re-reviewed between model runs, in which case the review tables in our
+#   data lake would not exactly match the review data that existed at the
+#   time of a historical model run.)
 tictoc::tic("Training data pulled")
 training_data <- dbGetQuery(
   conn = AWS_ATHENA_CONN_NOCTUA, glue("
@@ -96,11 +128,16 @@ training_data <- dbGetQuery(
       sale.deed_type AS meta_sale_deed_type,
       sale.seller_name AS meta_sale_seller_name,
       sale.buyer_name AS meta_sale_buyer_name,
-      sale.sv_is_outlier,
-      sale.sv_outlier_reason1,
-      sale.sv_outlier_reason2,
-      sale.sv_outlier_reason3,
-      sale.sv_run_id,
+      sale.is_outlier AS sv_is_outlier,
+      sale.outlier_reason AS sv_outlier_reason,
+      sale.flag_outlier_reason1 AS sv_outlier_reason1,
+      sale.flag_outlier_reason2 AS sv_outlier_reason2,
+      sale.flag_outlier_reason3 AS sv_outlier_reason3,
+      sale.flag_run_id AS sv_run_id,
+      CASE
+        WHEN sale.has_review
+          THEN review_json
+      END AS sv_review_json,
       res.*
   FROM model.vw_card_res_input res
   INNER JOIN default.vw_pin_sale sale
@@ -333,11 +370,6 @@ training_data_clean <- training_data_w_hie %>%
     any_of(col_type_dict$var_name),
     ~ recode_column_type(.x, cur_column())
   )) %>%
-  # Only exclude explicit outliers from training. Sales with missing validation
-  # outcomes will be considered non-outliers
-  mutate(
-    sv_is_outlier = replace_na(sv_is_outlier, FALSE)
-  ) %>%
   mutate(
     # Miscellaneous column-level cleanup
     ccao_is_corner_lot = replace_na(ccao_is_corner_lot, FALSE),
