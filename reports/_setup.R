ecee40c97ca0d64b599fd7d333a6966d12e03de5
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-02-19 16:24:53 -0600

    Update reports/_setup.R
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- reports/_setup.R
+++ reports/_setup.R
@@ -9,8 +9,11 @@ library(here)
 
 # We want sub-reports to be able to be run on their own. This ensures
 # that if `model_features.qmd` isn't the report run and no param is created,
-# we create the params object from the frontmatter of the main report file
-
+# we create the params object from the frontmatter of the main report file.
+#
+# WARNING: This function definition is a duplicate of a function with the same
+# name in `reports/model_features/_utils.R`, so if you change this function,
+# you should change that one too
 parse_params_from_frontmatter <- function(path, defaults = NULL) {
   fm <- rmarkdown::yaml_front_matter(path)
 
