e1e5897dd5bd99469ffae64f49d9fb4161a10bf1
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-02-20 16:42:39 -0600

    Update reports/model_features/_utils.R
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- reports/model_features/_utils.R
+++ reports/model_features/_utils.R
@@ -37,7 +37,9 @@ parse_params_from_frontmatter <- function(path, defaults = NULL) {
 
 # We only want to parse the params if they are not-defined
 if (!exists("params")) {
-  params <- parse_params_from_frontmatter("model_features.qmd")
+  params <- parse_params_from_frontmatter(
+    here::here("reports", "model_features", "model_features.qmd")
+  )
 }
 
 # Text sizes for small multiples
