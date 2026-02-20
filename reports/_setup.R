b29ef123713d458e01925b8e05b886b9f5e889e0
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-02-20 16:42:19 -0600

    Update reports/_setup.R
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- reports/_setup.R
+++ reports/_setup.R
@@ -32,7 +32,7 @@ parse_params_from_frontmatter <- function(path, defaults = NULL) {
 }
 
 # We only want to parse the params if they are not-defined
-if (!exists("params", inherits = TRUE)) {
+if (!exists("params")) {
   params <- parse_params_from_frontmatter("performance.qmd")
 }
 
