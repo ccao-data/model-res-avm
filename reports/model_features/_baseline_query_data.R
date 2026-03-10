ecccaaf124a68a07b3077bc515315f65014d6825
Damonamajor <damon.major@cookcountyil.gov>
2026-03-10 20:26:52 +0000

    SHAP handeling

--- reports/model_features/_baseline_query_data.R
+++ reports/model_features/_baseline_query_data.R
@@ -119,7 +119,9 @@ if (!exists("assessment_data_new")) {
 }
 
 # SHAPs
-if (file.exists(paths$output$shap$local) && metadata$shap_enable) {
+if (!exists("new_shaps") &&
+  file.exists(paths$output$shap$local) && # nolintr
+  metadata$shap_enable) {
   shap_df <- read_parquet(paths$output$shap$local)
   shap_exists <- nrow(shap_df) > 0
 
