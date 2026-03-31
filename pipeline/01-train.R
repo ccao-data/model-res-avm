4382c060e9c8d189ee411776c9d00cf7e0998f8a
Tim Sparer <tispare@ccao-datals.ccao.local>
2026-03-31 01:39:39 +0000

    Fix line length and linting issues

--- pipeline/01-train.R
+++ pipeline/01-train.R
@@ -393,32 +393,33 @@ walk2(
   list(test, train),
   list(paths$output$test_card$local, paths$output$train_card$local),
   \(data, path) {
-  data %>%
-    mutate(
-      pred_card_initial_fmv = predict(lgbm_wflow_final_fit, data)$.pred,
-      pred_card_initial_fmv_lin = exp(predict(
-        lin_wflow_final_fit,
-        data %>% mutate(meta_sale_price = log(meta_sale_price))
-      )$.pred)
-    ) %>%
-    select(
-      meta_year, meta_pin, meta_class, meta_card_num, meta_triad_code,
-      all_of(params$ratio_study$geographies), char_bldg_sf,
-      all_of(c(
-        "prior_far_tot" = params$ratio_study$far_column,
-        "prior_near_tot" = params$ratio_study$near_column
-      )),
-      pred_card_initial_fmv, pred_card_initial_fmv_lin,
-      meta_sale_price, meta_sale_date, meta_sale_document_num
-    ) %>%
-    # Prior year values are AV, not FMV. Multiply by 10 to get FMV for residential
-    mutate(
-      prior_far_tot = prior_far_tot * 10,
-      prior_near_tot = prior_near_tot * 10
-    ) %>%
-    as_tibble() %>%
-    write_parquet(path)
-}
+    data %>%
+      mutate(
+        pred_card_initial_fmv = predict(lgbm_wflow_final_fit, data)$.pred,
+        pred_card_initial_fmv_lin = exp(predict(
+          lin_wflow_final_fit,
+          data %>% mutate(meta_sale_price = log(meta_sale_price))
+        )$.pred)
+      ) %>%
+      select(
+        meta_year, meta_pin, meta_class, meta_card_num, meta_triad_code,
+        all_of(params$ratio_study$geographies), char_bldg_sf,
+        all_of(c(
+          "prior_far_tot" = params$ratio_study$far_column,
+          "prior_near_tot" = params$ratio_study$near_column
+        )),
+        pred_card_initial_fmv, pred_card_initial_fmv_lin,
+        meta_sale_price, meta_sale_date, meta_sale_document_num
+      ) %>%
+      # Prior year values are AV, not FMV.
+      # Multiply by 10 to get FMV for residential
+      mutate(
+        prior_far_tot = prior_far_tot * 10,
+        prior_near_tot = prior_near_tot * 10
+      ) %>%
+      as_tibble() %>%
+      write_parquet(path)
+  }
 )
 
 
