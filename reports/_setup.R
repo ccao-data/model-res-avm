7f9cf0617b310e7bd44acc7065dc08d14e990ef9
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-05-22 09:21:27 -0500

    Update _setup.R

--- reports/_setup.R
+++ reports/_setup.R
@@ -236,7 +236,7 @@ m_test_split_prop <- scales::percent(
   accuracy = 0.01
 )
 m_test_split_stratified_prop <- scales::percent(
-  as.numeric(metadata$cv$stratified_prop),
+  as.numeric(metadata$cv_stratified_prop),
   accuracy = 0.01
 )
 m_train_min_date <- min(training_data$meta_sale_date)
