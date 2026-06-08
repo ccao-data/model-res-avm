2ee30762562a9ebe5d38aa9dfa47213be9d62c5f
Damonamajor <damon.major@cookcountyil.gov>
2026-06-08 17:01:19 +0000

    revert to old stratification structure

--- pipeline/01-train.R
+++ pipeline/01-train.R
@@ -56,11 +56,10 @@ if (params$cv$stratified_prop != 0) {
     initial_split(
       prop = 1 - params$cv$stratified_prop,
       strata = .strat
-    ) %>%
-    select(-.strat)
+    )
 
-  train <- training(strat_split)
-  stratified_sample <- testing(strat_split)
+  train <- training(strat_split) %>% select(-.strat)
+  stratified_sample <- testing(strat_split) %>% select(-.strat)
   test <- bind_rows(test, stratified_sample)
 }
 
