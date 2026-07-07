637b8827e30e09b743561c56eba87dc14680ce54
Damonamajor <damon.major@cookcountyil.gov>
2026-07-07 18:13:44 +0000

    revert back to just changing output

--- R/helpers.R
+++ R/helpers.R
@@ -34,12 +34,10 @@ model_file_dict <- function(
     na.strings = ""
   ) %>%
     mutate(
-      s3_bucket = case_when(
-        use_dev_s3_paths & s3_bucket == "ccao-model-results-us-east-1" ~
-          "z-dev-ccao-model-results-us-east-1",
-        use_dev_s3_paths & s3_bucket == "ccao-data-dvc-us-east-1" ~
-          "z-dev-ccao-data-dvc-us-east-1",
-        .default = s3_bucket
+      s3_bucket = ifelse(
+        use_dev_s3_paths & s3_bucket == "ccao-model-results-us-east-1",
+        "z-dev-ccao-model-results-us-east-1",
+        s3_bucket
       ),
       s3 = as.character(purrr::map_if(
         path_s3, ~ !is.na(.x), glue::glue,
