595be56b54ddb2652b4e531c8ffbfcf280937188
Damonamajor <damon.major@cookcountyil.gov>
2026-09-02 19:01:45 +0000

    add select_all

--- pipeline/07-export.R
+++ pipeline/07-export.R
@@ -677,7 +677,10 @@ for (town in unique(assessment_pin_prepped$township_code)) {
     # This select statement aligns the card dataframe with the card schema
     # total_mv and mv_difference are written separately
     # via writeFormula below to populate the pivot table
-    select(setdiff(names(pin_detail_schema), c("total_mv", "mv_difference")))
+    select(all_of(setdiff(
+      names(pin_detail_schema),
+      c("total_mv", "mv_difference")
+    )))
 
   # Load the excel workbook template from file
   wb <- loadWorkbook(here("misc", "desk_review_template.xlsx"))
